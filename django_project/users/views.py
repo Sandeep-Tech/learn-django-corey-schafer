84bc1b82d965949ff029bc10f6d1625fa0394f9d
Sandeep Sharma <sandeeptech8@gmail.com>
2023-05-28 18:00:27 +0530

    fix error: not calling the the is_valid on form, only referring

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -9,7 +9,7 @@ def register(request):
     '''register View for a new user'''
     if request.method == "POST":
         form = UserCreationForm(request.POST)
-        if form.is_valid:
+        if form.is_valid():
             username = form.cleaned_data.get('username')
             messages.success(request, f"Account created for {username}")
             return redirect("blog-home")
